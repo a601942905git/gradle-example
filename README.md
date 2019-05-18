aa83ac7f6f5bbcf6f06b82edd868a2ab8bea114c
lipeng <lipeng@shein.com>
2019-05-18 14:32:38 +0800

    spring Boot Gradle 实现依赖的统一管理和版本的统一管理

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gradle-example
+Spring Boot Gradle 多模块实现，并通过Spring Boot Gradle插件完成依赖功能，实现依赖的统一管理和依赖版本号的统一管理
--- gradle-example-core/src/main/java/com/gradle/example/HelloServiceImpl.java
+++ gradle-example-core/src/main/java/com/gradle/example/HelloServiceImpl.java
@@ -1,8 +1,11 @@
 package com.gradle.example;
 
+import com.google.common.collect.Lists;
 import com.grable.example.HelloService;
 import com.grable.example.Person;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * com.gradle.example.HelloServiceImpl
@@ -20,10 +23,14 @@ public class HelloServiceImpl implements HelloService {
 
     @Override
     public Person getPerson() {
+        List<Person> list = Lists.newArrayList();
+
         Person person = Person.builder()
                 .id(10001)
                 .name("test")
                 .build();
+        list.add(person);
+        list.stream().forEach(System.out::println);
         return person;
     }
 }
